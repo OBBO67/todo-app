f723a8e840ab8c3ca5ef02a71f8d65d979f7b728
Owen Brady <KMDV47@motorolasolutions.com>
2020-04-20 19:21:17 +0100

    Adding error handler route. Adding user schema. Adding more dependencies.

--- todo-api/index.js
+++ todo-api/index.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 require("dotenv").config(); // load .env contents into process.env
 const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const todoRoutes = require("./routes/todos");
+const errorHandler = require("./handlers/error");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/api/todos", todoRoutes);
+
+app.use(function (req, res, next) {
+  let error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
--- todo-frontend/src/app/models/user.ts
+++ todo-frontend/src/app/models/user.ts
@@ -3,7 +3,5 @@ export class User {
   username: string;
   email: string;
   password: string;
-  firstName: string;
-  lastName: string;
   token?: string; // optional
 }
